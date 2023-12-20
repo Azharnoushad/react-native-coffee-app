974645388f45cc87a22317c17fb9af42af930aeb
Azharnoushad <azharnoushad@gmail.com>
2023-12-20 09:15:07 +0530

    bottom tab navigator is implimented

---
+++ CoffeeApp/src/navigators/TabNavigator.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import CoustomIcon from '../components/CoustomIcon';
+import {COLORS} from '../theme/theme';
+import {BlurView} from '@react-native-community/blur';
+import CartScreen from '../screens/CartScreen';
+import FavoriteScreen from '../screens/FavoriteScreen';
+import OrderHistoryScreen from '../screens/OrderHistoryScreen';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: false,
+        tabBarStyle: styles.tabBarStyle,
+        tabBarShowLabel: false,
+        tabBarBackground: () => (
+          <BlurView overlayColor="" blurAmount={15} style={styles.BlurView} />
+        ),
+      }}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <CoustomIcon
+              name="home"
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+              size={25}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="CartScreen"
+        component={CartScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <CoustomIcon
+              size={25}
+              name="cart"
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="FavoriteScreen"
+        component={FavoriteScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <CoustomIcon
+              name="like"
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="OrderHistoryScreen"
+        component={OrderHistoryScreen}
+        options={{
+          tabBarIcon: ({focused, size, color}) => (
+            <CoustomIcon
+              name="bell"
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    height: 80,
+    position: 'absolute',
+    backgroundColor: COLORS.primaryBlackRGBA,
+    borderTopWidth: 0,
+    elevation: 0,
+    borderTopColor: 'transparent',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    overflow: 'hidden',
+  },
+  BlurView: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+});
+export default TabNavigator;
